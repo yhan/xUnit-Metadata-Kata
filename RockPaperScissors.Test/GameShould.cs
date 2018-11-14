87ca30078a59b5d287bf00fe79d5f8424ba26b9f
FAMIBELLE Sebastien <sfamibelle@lafrancaise-group.com>
2018-11-14 10:20:17 +0100

    Create unit test

---
+++ RockPaperScissors.Test/GameShould.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace RockPaperScissors.Test
+{
+    public class GameShould
+    {
+        [Test]
+        public void Return_1_player_1_wins_game_When_draw_Rock_Scissors_Rock_Scissors()
+        {
+            SpyGameListener listener = new SpyGameListener();
+            Game game = new Game(listener);
+            game.PlayRound("Rock", "Scissors");
+            game.PlayRound("Rock", "Scissors");
+            Check.That(listener.Winner).IsEqualTo(1);
+        }
+
+        [Test]
+        public void Return_2_player_2_wins_game_When_draw_Rock_Paper_Rock_Paper()
+        {
+            SpyGameListener listener = new SpyGameListener();
+            var game = new Game(listener);
+            game.PlayRound("Rock", "Paper");
+            game.PlayRound("Rock", "Paper");
+            Check.That(listener.Winner).IsEqualTo(2);
+        }
+
+        [Test]
+        public void Return_0_drawers_not_counted_When_draw_Rock_Rock_Rock_Rock()
+        {
+            SpyGameListener listener = new SpyGameListener();
+            var game = new Game(listener);
+            game.PlayRound("Rock", "Rock");
+            game.PlayRound("Rock", "Rock");
+            Check.That(listener.Winner).IsEqualTo(0);
+        }
+
+        [Test]
+        public void Return_0_not_counted_When_invalid_moves()
+        {
+            var listener = new SpyGameListener();
+            var game = new Game(listener);
+            try
+            {
+                game.PlayRound("Blah", "Foo");
+                game.PlayRound("Rock", "Scissors");
+            }
+            catch (Exception e)
+            {
+
+            }
+            Check.That(listener.Winner).IsEqualTo(0);
+        }
+    }
+}
---
+++ RockPaperScissors.Test/RoundShould.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace RockPaperScissors.Test
+{
+    public class RoundShould
+    {
+        [Test]
+        public void Return_1_rock_blunts_scissors_PASS_When_draw_Rock_Scissors()
+        {
+            int result = new Round().Play("Rock", "Scissors");
+            Check.That(result).IsEqualTo(1);
+        }
+
+        [Test]
+        public void Return_2_rock_blunts_scissors_PASS_When_draw_Scissors_Rock()
+        {
+            int result = new Round().Play("Scissors", "Rock");
+            Check.That(result).IsEqualTo(2);
+        }
+
+        [Test]
+        public void Return_1_scissors_cut_paper_PASS_When_draw_Scissors_Paper()
+        {
+            int result = new Round().Play("Scissors", "Paper");
+            Check.That(result).IsEqualTo(1);
+        }
+
+        [Test]
+        public void Return_2_scissors_cut_paper_PASS_When_draw_Paper_Scissors()
+        {
+            int result = new Round().Play("Paper", "Scissors");
+            Check.That(result).IsEqualTo(2);
+        }
+
+        [Test]
+        public void Return_1_paper_wraps_rock_PASS_When_draw_Paper_Rock()
+        {
+            int result = new Round().Play("Paper", "Rock");
+            Check.That(result).IsEqualTo(1);
+        }
+
+        [Test]
+        public void Return_2_paper_wraps_rock_PASS_When_draw_Rock_Paper()
+        {
+            int result = new Round().Play("Rock", "Paper");
+            Check.That(result).IsEqualTo(2);
+        }
+
+
+        [Test]
+        public void Return_0_round_is_a_draw_When_draw_Rock_Rock()
+        {
+            int result = new Round().Play("Rock", "Rock");
+            Check.That(result).IsEqualTo(0);
+        }
+
+        [Test]
+        public void Return_0_round_is_a_draw_When_draw_Scissors_Scissors()
+        {
+            int result = new Round().Play("Scissors", "Scissors");
+            Check.That(result).IsEqualTo(0);
+        }
+
+        [Test]
+        public void Return_0_round_is_a_draw_When_draw_Paper_Paper()
+        {
+            int result = new Round().Play("Paper", "Paper");
+            Check.That(result).IsEqualTo(0);
+        }
+
+        [Test]
+        public void Return_InvalidMoveException_When_invalid_inputs()
+        {
+            Check.ThatCode(() => new Round().Play("Blah", "Foo")).Throws<InvalidMoveException>();
+        }
+    }
+}
